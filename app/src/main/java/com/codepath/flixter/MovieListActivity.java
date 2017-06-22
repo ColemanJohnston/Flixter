310a16a3cf8b2e1c7fc53a01d2abd8ee2edb2c30
Coleman Johnston <colemanmav@fb.com>
2017-06-21 17:14:46 -0700

    Fetched list of movies from endpoint

--- app/src/main/java/com/codepath/flixter/MovieListActivity.java
+++ app/src/main/java/com/codepath/flixter/MovieListActivity.java
@@ -49,7 +49,7 @@ public class MovieListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_list);
 
         client = new AsyncHttpClient();
-
+        movies = new ArrayList<Movie>();
         getConfiguration();
     }
 
@@ -69,11 +69,13 @@ public class MovieListActivity extends AppCompatActivity {
                     JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
 
                     posterSize = posterSizeOptions.optString(3,"w342");
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and posterSize %s", imageBaseUrl, posterSize));
 
+                    //get the now playing movie list after getting the configuration
+                    getNowPlaying();
                 } catch (JSONException e) {
                     logError("Failed parsing configuration", e, true);
                 }
-
             }
 
             @Override
@@ -83,6 +85,38 @@ public class MovieListActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void getNowPlaying(){
+        String url = API_BASE_URL + "/movie/now_playing";
+
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));
+
+        client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray results = response.getJSONArray("results");
+
+                    for(int i = 0; i < results.length(); ++i){
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                    }
+                    Log.i(TAG,String.format("Loaded  %s movies", results.length()));
+
+                } catch (JSONException e) {
+                    logError("Failed to parse now playing movies", e, true);
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get data from now_playing endpoint", throwable, true);
+            }
+        });
+    }
+
 
     private void logError(String message, Throwable error, boolean alertUser){
         Log.e(TAG, message, error);
